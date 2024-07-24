6d828e30235bfa3fafd3626b4c2b482ea3821de7
viraj1607 <merai.viraj@gmail.com>
2024-07-24 11:45:21 -0400

    api for hotel details

--- routes/hotel.js
+++ routes/hotel.js
@@ -30,7 +30,7 @@ router.get("/", async (req, res) => {
   }
 });
 
-router.get('/api/hotels/:id', async (req, res) => {
+router.get('/:id', async (req, res) => {
   const { id } = req.params;
 
   try {
